83739af2c79d4138c6d8dda4615dd238d45505e7
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-04 22:18:28 +0400

    added styles

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import Header from '../components/header'
-import Footer from '../components/footer'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-     <h1>Главная</h1>
-     <Link href='/burgers'><a>Все бургеры</a></Link>
-     <Footer/>
+    <div className={styles.container}>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
+      <p className={styles.text}>
+        Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
+        мясо. О других составляющих начинки можно поспорить, ведь это дело
+        вкуса.
+      </p>
+      <p className={styles.text}>
+        Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+        обслуживания, правильная атмосфера заведения.
+      </p>
+      <Link href="/burgers/">
+        <a className={styles.btn}>Все бургеры</a>
+      </Link>
     </div>
-  )
+  );
 }
