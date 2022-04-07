9f6fa1c1658cb60b5e6c02ce5e9e4c52b88a5cdb
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-07 11:45:36 +0400

    redirect

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const NotFoundPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Ой</h1>
       <h2>Такой страницы здесь нет!</h2>
       <p>
-        Прейти на
+        Перехожу на
         <Link href="/">
-          <a>главную страницу</a>
+          <a>главную страницу через 3 секунды...</a>
         </Link>
       </p>
     </div>
