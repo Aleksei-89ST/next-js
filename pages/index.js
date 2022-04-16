c37cfe6719649a6f07921cf81d9a19b63ff580e5
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-16 12:59:37 +0400

    использовал специальные компоненты Image и Head

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,16 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Жирные бургеры | Главная</title>
+      <mete name='title' content='Жирные бургеры'/>
+    </Head>
     <div className={styles.container}>
       <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
+      <div className={styles.mainImage}>
+        <Image src='/fatburger.png' alt='fat burger' width={400} height={300}/>
+      </div>
       <p className={styles.text}>
         Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
         мясо. О других составляющих начинки можно поспорить, ведь это дело
@@ -20,5 +28,6 @@ export default function Home() {
         <a className={styles.btn}>Все бургеры</a>
       </Link>
     </div>
+    </>
   );
 }
