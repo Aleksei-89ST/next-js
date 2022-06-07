9337cf54117cb54f36f03c60ddd48e9a65e70a3b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-07 21:49:49 +0400

    Отображаю список бургеров на странице

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,15 +1,43 @@
-export const getStaticProps = async () => {
-  const res = await fetch ("http://localhost:5000/items")
-  const data = await res.json()
-  return {
-    props: {burgers:data}
-  }
-}
+import Link from "next/dist/client/link";
+import styles from "../../styles/Burgers.module.css";
+import Image from "next/dist/client/image";
 
-const Burgers = (props) => {
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/items");
+  const data = await res.json();
+  return {
+    props: { burgers: data },
+  };
+};
+
+const Burgers = ({ burgers }) => {
   return (
     <div>
       <h1>Наши Бургеры</h1>
+      {burgers.map((burger) => {
+        return (
+          <Link href={`/burgers/${burger.id}`} key={burger.id}>
+            <a className={styles.burgerCard}>
+              <div className={styles.imageContainer}>
+                <Image
+                src={`${burger.image}`}
+                alt={`${burger.name}`}
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="cover"
+                />
+              </div>
+              <div>
+                <h3>
+                  {burger.name}
+                  <p>{burger.desc}</p>
+                </h3>
+              </div>
+            </a>
+          </Link>
+        );
+      })}
     </div>
   );
 };
